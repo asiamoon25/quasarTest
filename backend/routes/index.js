9e4aca7f0764800456c42eb598f85efbb02396eb
HAPPY\LENOVO <wjddustkd45@gmail.com>
2022-03-08 17:29:30 +0900

    mongo db 연동

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,8 +4,8 @@ var path = require('path');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.sendFile(path.join(__dirname,'../public/index.html'))
-
+  res.sendFile(path.join(__dirname,'../views/index.jade'))
 });
+
 
 module.exports = router;
