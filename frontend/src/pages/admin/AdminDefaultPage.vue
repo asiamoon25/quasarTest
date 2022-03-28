18d6ea3264d73481712267c236b2296ab495a510
정연상 <wjddustkd45@gmail.com>
2022-03-28 18:20:18 +0900

    [front] AdminDefaultPage.vue 화면 추가

---
+++ frontend/src/pages/admin/AdminDefaultPage.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+<script>
+import {computed, onBeforeMount, reactive} from "vue";
+
+export default {
+  name : 'AdminDefaultPage',
+  setup() {
+
+    const state = reactive({
+
+    })
+    return {
+      state
+    }
+  }
+}
+</script>
+<style>
+
+</style>
